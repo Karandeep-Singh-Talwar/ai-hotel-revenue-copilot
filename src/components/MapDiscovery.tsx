725e9dc6939902b80448a88238b786cd8729e5eb
Karandeep Singh Talwar <karandeepstalwar@gmail.com>
2026-09-03 14:47:53 +0530

    fix: removed unused useEffect and disabled eslint for Leaflet prototype mutation

--- src/components/MapDiscovery.tsx
+++ src/components/MapDiscovery.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 // Fix for default marker icons in Leaflet with Next.js/Webpack
+// @ts-ignore
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
